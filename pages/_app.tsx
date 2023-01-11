410185e1ef067b37efa790155001d0c614388c63
suld2495 <suld7777@gmail.com>
2023-01-11 22:32:05 +0900

    [add] 환경설정 추가
    1. msw 설치
    2. import 기본 경로 설정
    3. eslint 룰 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import '../styles/globals.css';
+import 'styles/globals.css';
 import type { AppProps } from 'next/app';
-import {
-  QueryClient,
-  Hydrate,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-// eslint-disable-next-line import/no-extraneous-dependencies
+import { QueryClient, Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
+
+if (process.env.NODE_ENV === 'development') {
+  import('mocks/browser').then(({ worker }) => {
+    worker.start();
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(new QueryClient());
